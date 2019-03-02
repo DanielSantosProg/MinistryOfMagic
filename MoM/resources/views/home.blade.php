27a449cb94d4bc71395c09cfaaea668fd56f672a
Daniel Silva Santos <danielsantos_lol@hotmail.com>
2019-03-02 11:18:02 -0300

    Adicionado paginas de cadastro(funcionario), home e alteracoes na pagina de login

---
+++ MoM/resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layout')
+
+@section('content')
+    <div id="imglogo">
+            <img src="/imgs/logo_g.png" class="img2">
+    </div>
+    <button type="button" class="btn btn-outline-primary" id="home_but_1">Cadastrar sala</button>
+    <button type="button" class="btn btn-outline-primary" id="home_but_2">Alocar funcionário</button>
+    <button type="button" class="btn btn-outline-primary" id="home_but_3">Cadastrar departamento</button>
+    <button type="button" class="btn btn-outline-primary" id="home_but_4">Exibir funcionários</button>
+@endsection
--- MoM/routes/web.php
+++ MoM/routes/web.php
@@ -16,9 +16,13 @@ Route::get('/', function () {
 });
 
 Route::get('/home', function () {
-    return view('layout');
+    return view('home');
 });
 
 Route::get('/login', function () {
     return view('login');
+});
+
+Route::get('/cadastro', function () {
+    return view('cadastro_funcionario');
 });
