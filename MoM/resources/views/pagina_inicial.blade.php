cb54def2674281648014bf801c98fae6ec11d531
No one <noone@nothing.com>
2019-03-18 20:39:37 -0300

    Carousel da página inicial adicionado

--- MoM/resources/views/pagina_inicial.blade.php
+++ MoM/resources/views/pagina_inicial.blade.php
@@ -11,8 +11,29 @@
 <body>
     <div id="imglogo">
         <img src="/imgs/logo_g.png" class="img1">
+    </div>   
+    <div>
+        <button type="button" class="btn btn-outline-primary" id="login_but">Login</button>
+        <button type="button" class="btn btn-outline-primary" id="reg_but">Registrar funcionário</button>
     </div>
-    <button type="button" class="btn btn-outline-primary" id="login_but">Login</button>
-    <button type="button" class="btn btn-outline-primary" id="reg_but">Registrar funcionário</button>
+    <div id="carouselExampleIndicators" class="carousel slide mycarousel" data-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img class="d-block w-100 imgcar" src="\imgs\varinhas.jpg" alt="First slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100 imgcar" src="\imgs\dumbledore.jpg" alt="Second slide">
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+    </div>     
+      <script type="text/javascript" src="{{asset('js/app.js')}}"></script>  
 </body>
 </html>
