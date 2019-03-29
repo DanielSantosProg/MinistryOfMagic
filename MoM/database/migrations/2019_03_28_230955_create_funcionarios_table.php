158e6b3f73266aab8d944764efe0ade2693b2068
Daniel Silva Santos <danielsantos_lol@hotmail.com>
2019-03-28 21:18:09 -0300

    Alterações Navbar

---
+++ MoM/database/migrations/2019_03_28_230955_create_funcionarios_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFuncionariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('funcionarios', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome');
+            $table->string('sexo');
+            $table->string('endereço');
+            $table->string('foto');
+            $table->string('rub');
+            $table->string('posição');
+            $table->bigInteger('id_departamento')->unsigned();
+            $table->foreign('id_departamento')->references('id')->on('departamentos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('funcionarios');
+    }
+}
