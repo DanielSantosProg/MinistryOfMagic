29b348639b78c5a17a0caab0afaf9aad270d68b9
Daniel Silva Santos <danielsantos_lol@hotmail.com>
2019-04-02 00:08:36 -0300

    Alterações em rotas, controller de funcionários e css

--- MoM/resources/views/components/navbar.blade.php
+++ MoM/resources/views/components/navbar.blade.php
@@ -20,7 +20,7 @@
           </div>
         </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Alocar Funcionário</a>
+        <a class="nav-link" href="/alocarfuncionario">Alocar Funcionário</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="/exibir">Exibir Funcionários</a>
