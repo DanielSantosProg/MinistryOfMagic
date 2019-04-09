834fdfcc0054ac98435b6637479638f9bc98cb2c
Felipe Moura <felipemmoura0397@gmail.com>
2019-04-09 20:23:16 -0300

    correções de erros

--- MoM/resources/views/sala_editar.blade.php
+++ MoM/resources/views/sala_editar.blade.php
@@ -1,6 +1,7 @@
 @extends('layout')
 
 @section('content')
+<div class='rectangle'>
     <form action = "{{ route('salas.update', $sala) }}" method = "POST">
             @csrf
             @method('PUT')
@@ -13,9 +14,9 @@
                 <h2 id='nummaxfun'>Quantidade de pessoas</h2>
                 <div>
                     <input type = "text" class = "form-control" id="dadonumsala2" name="qtd_pessoas" placeholder = "Digite a quantidade máxima de pessoas aqui">
-
                 </div>
                 <button type="submit" class="btn btn-outline-primary" id="salaEdit_but">Editar</button>
             </div>
     </form>
+</div>
 @endsection
--- MoM/resources/views/salas_listar.blade.php
+++ MoM/resources/views/salas_listar.blade.php
@@ -2,30 +2,30 @@
 
 @section('content')
 <table class="table table-hover">
-	<thead>
-		<tr>
-			<th scope="col">ID</th>
-			<th scope="col">Numero</th>
-			<th scope="col">Qtd_pessoas</th> 
-		</tr>
-	</thead>
-	<tbody>
-		@foreach ($sala as $salalist)
-		<td>{{ $salalist->id }}</td>
-                <td>{{ $salalist->Numero }}</td>
-                <td>{{ $salalist->Qtd_pessoas }}</td>
-                <td>
-                  <form action = "{{ route('sala.destroy', $salalist->id) }}" method = "POST">
-                    @csrf
-                    @method('DELETE')
-                    <button type = "submit" class="btn btn-danger">Excluir</button>
-                  </form>
-                </td>
-                </td>                   
-            </tr>          
-            @endforeach   
-	</tbody>
-
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Numero</th>
+      <th scope="col">Qtd_pessoas</th> 
+    </tr>
+  </thead>
+  <tbody>
+    @foreach ($salas as $salalist)
+    <tr>
+        <td>{{ $salalist->id }}</td>
+        <td>{{ $salalist->numero }}</td>
+        <td>{{ $salalist->qtd_pessoas }}</td>
+        <td>
+          <form action = "{{ route('salas.destroy', $salalist->id) }}" method = "POST">
+            @csrf
+            <a href ="{{ route('salas.edit', $salalist->id) }}" class="btn btn-success">Editar</a>
+            @method('DELETE')
+            <button type = "submit" class="btn btn-danger">Excluir</button>
+          </form>
+        </td>
+        </td>                   
+    </tr>         
+    @endforeach   
+  </tbody>
 </table>
 @endsection
-
